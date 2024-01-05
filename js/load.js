22c51c3d365aa801c005ceebc8a7e560fec823c6
Joan <joan.vigne@hotmail.fr>
2024-01-05 11:06:07 +0000

    fix load, fix delete

--- js/load.js
+++ js/load.js
@@ -1,5 +1,5 @@
 // MENU CONTINUE
-function continueGame() {
+/* function continueGame() {
   let saves = localStorage.getItem("saves");
   if (saves === null) {
     homeMadeAlert("No game saved yet", "press Start New Game to play");
@@ -8,6 +8,10 @@ function continueGame() {
 
   let menuButtonContainer = document.getElementsByTagName("fieldset");
   if (menuButtonContainer.length < 2) {
+    console.log(menuButtonContainer.length);
+    if (menuButtonContainer.length === 0) {
+      return;
+    }
     quitMenu();
   }
 
@@ -25,8 +29,8 @@ function continueGame() {
             <br>  `;
 
   gameSaved.forEach((element) => {
-    let oneElement = document.createElement("div");
-    oneElement.classList.add("savings");
+    let oneSaving = document.createElement("div");
+    oneSaving.classList.add("savings");
 
     let title = element[0];
     let location = element[1];
@@ -36,10 +40,10 @@ function continueGame() {
     let enemyArray = element[5];
 
     let space = " ";
-    let elementContent = document.createElement("input");
-    elementContent.type = "button";
+    let elementContainer = document.createElement("input");
+    elementContainer.type = "button";
 
-    elementContent.addEventListener("click", () =>
+    elementContainer.addEventListener("click", () =>
       load(title, location, JSON.parse(stuff), position, JSON.parse(enemyArray))
     );
 
@@ -49,9 +53,9 @@ function continueGame() {
     buttonDelet.addEventListener("click", () => {
       deletASaving(element);
     });
-    elementContent.value = title + space + timeOfSaving;
-    oneElement.append(elementContent, buttonDelet);
-    theGameSaved.append(oneElement);
+    elementContainer.value = title + space + timeOfSaving;
+    oneSaving.append(elementContainer, buttonDelet);
+    theGameSaved.append(oneSaving);
   });
 
   continueMenu.append(theGameSaved);
@@ -70,7 +74,90 @@ function continueGame() {
       }
     );
   }
+} */
+// creation var global au lieu de getItem tout le temps
+const myGames = JSON.parse(localStorage.getItem("saves"));
+console.log(myGames);
+// nouvelle
+function continueGame() {
+  if (myGames === null || myGames.length === 0) {
+    homeMadeAlert("No game saved yet", "press Start New Game to play");
+    return;
+  }
+  let menuButtonContainer = document.getElementsByTagName("fieldset");
+  if (menuButtonContainer.length < 2) {
+    console.log(menuButtonContainer.length);
+    if (menuButtonContainer.length === 0) {
+      return;
+    }
+    quitMenu();
+  }
+  const continueMenu = document.createElement("div");
+  continueMenu.setAttribute("id", "continueMenu");
+
+  let theGameSaved = document.createElement("div");
+  theGameSaved.setAttribute("id", "theGameSaved");
+
+  continueMenu.innerHTML = `<div id="continueTitleAndBack">
+  <img src="images/westArrow.png" alt="back" onClick="backFunction()">
+  <h5>Which save do you want to use?</h5></div>
+            <br>  `;
+
+  continueMenu.append(theGameSaved);
+  container.append(continueMenu);
+
+  displayMyGames();
+  /*   focusThisInput(0); */
 }
+function removeDisplayedGames() {
+  const allTheSavingDisplayed = document.querySelectorAll(".savings");
+  if (allTheSavingDisplayed.length === 0) {
+    return;
+  }
+  allTheSavingDisplayed.forEach((element) => {
+    element.remove();
+  });
+}
+function displayMyGames() {
+  removeDisplayedGames();
+  myGames.forEach((element) => {
+    let oneSaving = document.createElement("div");
+    oneSaving.classList.add("savings");
+    let title = element[0];
+    let location = element[1];
+    let stuff = element[2];
+    let timeOfSaving = element[3];
+    let position = element[4];
+    let enemyArray = element[5];
+    let elementContainer = document.createElement("input");
+    elementContainer.type = "button";
+    elementContainer.addEventListener("click", () =>
+      load(title, location, JSON.parse(stuff), position, JSON.parse(enemyArray))
+    );
+    const containerTimeOfSaving = document.createElement("p");
+    containerTimeOfSaving.innerHTML = timeOfSaving;
+    let buttonDelet = document.createElement("button");
+    buttonDelet.innerHTML = "x";
+    buttonDelet.setAttribute("class", "buttonDelet");
+    buttonDelet.addEventListener("click", () => {
+      confirmMessage(
+        "Do you really want to delete this save ?",
+        "yes, i am sure",
+        () => {
+          // modifier "myGames" plutot que gamesaved
+          myGames.splice(myGames.indexOf(element), 1);
+          closePromptBox();
+          localStorage.setItem("saves", JSON.stringify(myGames));
+          displayMyGames();
+        }
+      );
+    });
+    elementContainer.value = title;
+    oneSaving.append(elementContainer, containerTimeOfSaving, buttonDelet);
+    theGameSaved.append(oneSaving);
+  });
+}
+
 // LOAD AND START PLAYING
 function load(title, location, stuff, position, enemies) {
   let continueMenu = document.getElementById("continueMenu");
--- js/save.js
+++ js/save.js
@@ -62,5 +62,5 @@ function getFormattedDate() {
   const date = new Date();
   return `${date.getDate()}/${
     date.getMonth() + 1
-  }/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
+  }/${date.getFullYear()} </br> ${date.getHours()}:${date.getMinutes()}`;
 }
